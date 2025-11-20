592ac46bfc01927cfe29f214043beff90f5c0b13
daineko vitaly <daineko_v@ddc.by>
2025-11-20 17:05:49 +0300

    qwer

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,10 @@
 terraform {
   backend "s3" {
-    key     = "eks-dev/terraform.tfstate"
-    region  = "us-east-2"
-    encrypt = true
+    key            = "eks-dev/terraform.tfstate"
+    region         = "us-east-2"
+    encrypt        = true
+    dynamodb_table = "dynamodb-vitaly19112025"
+    bucket         = "terraform-vitaly19112025"
   }
 }
 
